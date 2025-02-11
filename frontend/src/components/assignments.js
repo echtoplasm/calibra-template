7e67a8c71259d2e694f0121b6787c563b04d3931
echtoplasm <zacharyamassey@gmail.com>
2025-02-11 18:34:13 -0500

    added png files for assignments

--- frontend/src/components/assignments.js
+++ frontend/src/components/assignments.js
@@ -6,9 +6,9 @@ const AssignmentLinks = () => {
       <h2>Web-Assignments</h2>
       <p>Hello Charlie!</p>
       <ul>
-        <li><a href="assingments/functions.png" target="blank" rel="noopener norefferer">Functions Assignment</a></li>
-        <li><a href="assingments/oop.png" target="_blank" rel="noopener norefferer">OOP assingment</a></li>
-        <li><a href="assingments/sessions.png" target="_blank" rel="noopener norefferer">Sessions</a></li>
+        <li><a href="assignments/functions.png" target="blank" rel="noopener norefferer">Functions Assignment</a></li>
+        <li><a href="assignments/oop.png" target="_blank" rel="noopener norefferer">OOP Assignment</a></li>
+        <li><a href="assignments/sessions.png" target="_blank" rel="noopener norefferer">Sessions Assignment</a></li>
       </ul>
     </div>
   );
